f0a7a99a13876617565d4420496203822e730a73
fakorede <abiolafakorede@gmail.com>
2020-07-02 13:35:55 +0100

    created User details model, migration and controller

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Welcome to CV Able</h1>
+    <button class="btn btn-primary">Build your resume now</button>
+</div>
+@endsection
